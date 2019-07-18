58ce340b25333f273335d32198d5e6428b009d90
Axel Cocat <ax.cocat@gmail.com>
2019-07-18 19:51:23 +0200

    Added trails

--- client/src/behaviours/Projectile.js
+++ client/src/behaviours/Projectile.js
@@ -25,12 +25,28 @@ export default class Projectile extends Behaviour {
         this.timer = 0;
         this.id = id;
 
+        this.trail = new THREE.TrailRenderer(scene, false);
+        var headGeometry = new THREE.SphereGeometry(size * 1.5, 4, 4);
+        var trailMaterial = THREE.TrailRenderer.createBaseMaterial();
+        if (team == 0) {
+            trailMaterial.uniforms.headColor.value.set(0.0, 0.53, 0.82, 0.1);
+            trailMaterial.uniforms.tailColor.value.set(0.2, 0.3, 0.8, 0.05);
+        } else {
+            trailMaterial.uniforms.headColor.value.set(1.0, 0.05, 0.05, 0.1);
+            trailMaterial.uniforms.tailColor.value.set(0.8, 0.2, 0.2, 0.05);
+        }
+        var trailLength = 10;
+        this.trail.initialize(trailMaterial, trailLength, false, 0, headGeometry.vertices, this.mesh);
+        this.trail.activate();
+
         scene.add(this.mesh);
     }
 
     destroy() {
         super.destroy();
         this.scene.remove(this.mesh);
+        this.trail.deactivate();
+        this.scene.remove(this.trail);
     }
 
     position(val) {
@@ -43,5 +59,9 @@ export default class Projectile extends Behaviour {
         } else {
             this.mesh.position.set(val.x, val.y, val.z);
         }
+    }
+
+    update() {
+        this.trail.advance();
     }
 }
